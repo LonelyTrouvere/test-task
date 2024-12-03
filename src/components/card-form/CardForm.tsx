dd93d5faf2d40e0b967ffb3bee6e2135b7535e22
Kobzar Stanislav <kobzar_stanislav@knu.ua>
2024-12-04 00:17:57 +0200

    added input validators

--- src/components/card-form/CardForm.tsx
+++ src/components/card-form/CardForm.tsx
@@ -2,6 +2,13 @@ import './CardForm.css'
 import LabeledInput from "../../utility-components/labeled-input/LabeledInput";
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { FormInputs } from './type';
+import cardValidator from 'card-validator';
+
+// validates {number}{number}/{number}{number}
+const dateRegex = /^\d{2}\/\d{2}/
+// validates 3 numbers in a row
+const cvcRegex = /^\d{3}$/
+
 function CardForm() {
     const { register, handleSubmit, formState: { errors } } = useForm<FormInputs>({
         shouldFocusError: false
@@ -9,13 +16,29 @@ function CardForm() {
     const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data)
 
     const cardRegister = register('card', {
-        required: 'Credit card number required'
+        required: 'Credit card number required',
+        validate: (value) => {
+            const validated = cardValidator.number(value)
+            if(!validated.isPotentiallyValid){
+                return 'Invalid credit card number'
+            }
+
+            return validated.isValid
+        }
     })
     const dateRegister = register('date', {
-        required: 'Expiration date required'
+        required: 'Expiration date required',
+        pattern: {
+            message: 'Invalid Expiration date',
+            value: dateRegex
+        }
     })
     const cvcRegister = register('cvc', {
-        required: 'CVC required'
+        required: 'CVC required',
+        pattern: {
+            message: 'Invalid CVC',
+            value: cvcRegex
+        }
     })
 
     return  <form className="payment-area__card" onSubmit={handleSubmit(onSubmit)}>
