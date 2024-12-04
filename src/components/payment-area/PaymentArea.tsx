1165a25ca82a5190e237d6f32ff99fb6bfdafe78
Kobzar Stanislav <kobzar_stanislav@knu.ua>
2024-12-04 22:32:24 +0200

    added apple pay button

--- src/components/payment-area/PaymentArea.tsx
+++ src/components/payment-area/PaymentArea.tsx
@@ -1,7 +1,9 @@
+import Button from '../../utility-components/button/Button'
 import LeveledSpan from '../../utility-components/leveled-span/LeveledSpan'
 import CardForm from '../card-form/CardForm'
 import LegalInfo from '../legal-info/LegalInfo'
 import './PaymentArea.css'
+import applePayLogo from '../../assets/apple-pay.svg'
 
 function PaymentArea() {
     return <div className="payment-area">
@@ -10,6 +12,7 @@ function PaymentArea() {
                 <LeveledSpan level='first' style={{fontSize: '32px', lineHeight: '40px', display: 'block'}}>5 days free</LeveledSpan>
                 <LeveledSpan level='second' style={{fontSize: '16px', lineHeight: '24px',}}>then 299.99 UAH per 14 days</LeveledSpan>
             </div>
+            <Button style={{color: 'white', backgroundColor: 'black'}}><img src={applePayLogo}/></Button>
             <CardForm />
         </div>
         <LegalInfo />
