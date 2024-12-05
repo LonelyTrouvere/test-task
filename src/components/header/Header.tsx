d9eb3fe989548c274e50378a7f7c3d7f1cd323c2
Kobzar Stanislav <kobzar_stanislav@knu.ua>
2024-12-05 02:52:42 +0200

    final component. design fully done

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,20 @@
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 import LanguagePicker from '../../utility-components/language-picker/LanguagePicker'
+import LeveledSpan from '../../utility-components/leveled-span/LeveledSpan'
 import './Header.css'
+import arrowSvg from '../../assets/arrow.svg'
 
 function Header() {
+    const { width } = useWindowDimensions();
     return <div className='header'>
+        {
+            width < 1000
+            ? <>
+                <img src={arrowSvg}/>
+                <LeveledSpan level='first'>Checkout</LeveledSpan>
+            </>
+            : <></>
+        }
         <LanguagePicker />
     </div>
 }
--- src/components/payment-area/PaymentArea.tsx
+++ src/components/payment-area/PaymentArea.tsx
@@ -4,11 +4,23 @@ import CardForm from '../card-form/CardForm'
 import LegalInfo from '../legal-info/LegalInfo'
 import './PaymentArea.css'
 import applePayLogo from '../../assets/apple-pay.svg'
+import arrowSvg from '../../assets/arrow.svg'
 import Divider from '../../utility-components/divider/Divider'
+import useWindowDimensions from '../../hooks/useWindowDimensions'
 
 function PaymentArea() {
+    const { width } = useWindowDimensions()
+
     return <div className="payment-area">
         <div className="payment-area__payment">
+            {
+                width >= 1000
+                ? <div className='payment-area__checkout'>
+                    <img className='payment-area__arrow' src={arrowSvg}/>
+                    <LeveledSpan level='first'>Checkout</LeveledSpan>
+                </div>
+                : <></>
+            }
             <div className="payment-area__order">
                 <LeveledSpan level='first' style={{fontSize: '32px', lineHeight: '40px', display: 'block'}}>5 days free</LeveledSpan>
                 <LeveledSpan level='second' style={{fontSize: '16px', lineHeight: '24px',}}>then 299.99 UAH per 14 days</LeveledSpan>
